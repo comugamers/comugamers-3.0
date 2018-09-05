22a97d4d12277af764fe64ffde497af89f5f6549
comugamers <43016488+comugamers@users.noreply.github.com>
2018-09-05 16:36:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
