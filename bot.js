8be3707ff3eab11e394432b1e2ef7fbbd34c9071
comugamers <43016488+comugamers@users.noreply.github.com>
2018-09-05 16:31:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+co﻿nst client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`I an ready!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`);
+});﻿
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
@@ -11,5 +11,4 @@ client.on('message', msg => {
   }
 });
 
-// THIS MUST BE THIS WAY
-client.login('process.env.BOT_token');
+client.login('token');
